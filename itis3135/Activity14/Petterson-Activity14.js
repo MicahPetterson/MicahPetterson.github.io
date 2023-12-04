b8de0144644fad880236c2de772a91d4ce30dc61
MicahPetterson <micahpetterson5@hotmail.com>
2023-12-04 17:57:26 -0500

    eee

--- itis3135/Activity14/Petterson-Activity14.js
+++ itis3135/Activity14/Petterson-Activity14.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-	$("#nav_list a").click(function() {
-		$.get(`json_files/${$(this).attr("title")}.json`).done(function(speaker) {
+$(document).ready(function()
+{
+	$("#nav_list a").click(function()
+	{
+		$.get(`json_files/${$(this).attr("title")}.json`).done(function(speaker)
+		{
 			let speakerInfo = speaker.speakers[0];
 			$("main h1").text(speakerInfo.title);
 			$("main h2").text(speakerInfo.month);
 			$("main h3").text(speakerInfo.speaker);
 			$("main img").attr("src", speakerInfo.image);
+			$("main img").attr("alt", speakerInfo.title + " image.");
 			$("main p").html(speakerInfo.text);
-		});
-	})
-}); // end ready
+		}
+		);
+	}
+	)
+}
+); // end ready
