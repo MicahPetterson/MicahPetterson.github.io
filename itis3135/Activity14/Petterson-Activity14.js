34480be760dd030441e9a41899d1af1368b6021a
MicahPetterson <micahpetterson5@hotmail.com>
2023-12-04 17:51:27 -0500

    speakers

---
+++ itis3135/Activity14/Petterson-Activity14.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$("#nav_list a").click(function() {
+		$.get(`json_files/${$(this).attr("title")}.json`).done(function(speaker) {
+			let speakerInfo = speaker.speakers[0];
+			$("main h1").text(speakerInfo.title);
+			$("main h2").text(speakerInfo.month);
+			$("main h3").text(speakerInfo.speaker);
+			$("main img").attr("src", speakerInfo.image);
+			$("main p").html(speakerInfo.text);
+		});
+	})
+}); // end ready
